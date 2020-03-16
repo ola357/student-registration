9c2af929c1cf674e9a45f12a26d8d7f2a599385c
ola357 <alliolaoluwa@gmail.com>
2020-03-16 10:41:31 +0100

    Initial commit

---
+++ src/main/java/com/ola/studentregistration/StudentRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.ola.studentregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ola/studentregistration/StudentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ola.studentregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
